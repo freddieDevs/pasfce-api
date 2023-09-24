2c66da786eafb0a2f4a71839cce13ed08b2a164b
freddieDevs <133527315+freddieDevs@users.noreply.github.com>
2023-09-24 04:08:13 +0300

    used the cyclic url as a variable

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen('https://long-jade-eagle-wear.cyclic.cloud');
+  await app.listen(process.env.CYCLIC_URL);
 }
 bootstrap();
