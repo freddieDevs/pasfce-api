2c427fab0ff2ebfdb8abfb671e3f1b8a933198da
freddieDevs <133527315+freddieDevs@users.noreply.github.com>
2023-09-28 04:28:49 +0300

    using vercel dev

--- src/api/main.ts
+++ src/api/main.ts
@@ -33,3 +33,16 @@ export const handler: Handler = async (
   server = server ?? (await bootstrap());
   return server(event, context, callback);
 };
+
+// import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+// export default function handler(
+//   request: VercelRequest,
+//   response: VercelResponse,
+// ) {
+//   response.status(200).json({
+//     body: request.body,
+//     query: request.query,
+//     cookies: request.cookies,
+//   });
+// }
